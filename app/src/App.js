dff6b1f8ee2a80790962b410e1785c0137480e89
Siniša Panić <sinisap15@gmail.com>
2023-03-25 13:28:00 +0100

    news app implemented

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,57 @@
 import React, { useState, useEffect } from 'react';
 
 const App = () => {
-	const [count, setCount] = useState(0);
+	// state
+	const [news, setNews] = useState([]);
+	const [searchQuery, setSearchQuery] = useState('react');
+	const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react');
+	const [loading, setLoading] = useState(false);
+	// fetch the news
+	const fetchNews = () => {
+		setLoading(true);
+		fetch(url)
+			.then(result => result.json())
+			.then(data => {
+				setNews(data.hits);
+				setLoading(false);
+			})
+			.catch(error => console.log(error));
+	};
 
-	// Instead of componentDidMount and componentDidUpdate:
 	useEffect(() => {
-		document.title = `Clicked ${count} times`;
-	})
+		fetchNews();
+	}, [url]);
 
-	const increment = () => {
-		setCount(count + 1);
+	const handleChange = e => {
+		setSearchQuery(e.target.value);
+	}
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+	}
+
+	const showLoading = () => (loading ? <h2>Loading...</h2> : "");
+
+	const searchForm = () => (
+		<form onSubmit={handleSubmit}>
+			<input type="text" value={searchQuery} onChange={handleChange} />
+			<button type='submit'>Search</button>
+		</form>
+	)
+
+	const showNews = () => {
+		return news.map((n, i) => (
+			<p key={i}>{n.title}</p>
+		))
 	}
 
 	return (
 		<div>
-			<h2>Counter app</h2>
-			<button onClick={increment}>
-				Clicked {count} times
-			</button>
+			<h2>News</h2>
+			{showLoading()}
+			{searchForm()}
+			{showNews()}
 		</div>
 	)
 }
