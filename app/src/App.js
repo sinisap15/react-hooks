d9804b1ff3afd1da64501273dabe9d16861f7033
Siniša Panić <sinisap15@gmail.com>
2023-03-25 12:49:15 +0100

    useState hook implemented

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [count, setCount] = useState(0);
+
+	const increment = () => {
+		setCount(count + 1);
+	}
+
+	return (
+		<div>
+			<h2>Counter app</h2>
+			<button onClick={increment}>
+				Clicked {count} times
+			</button>
+		</div>
+	)
 }
 
 export default App;
